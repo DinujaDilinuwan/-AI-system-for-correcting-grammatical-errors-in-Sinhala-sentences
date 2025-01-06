4de2584f37e573d65a27e92b0c90b248b7a8e339
NirupamaD99 <150535823+NirupamaD99@users.noreply.github.com>
2025-01-06 19:52:06 +0530

    edited

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: {}, // Use an object for turbo (even if empty)
-  },
-};
-
-module.exports = nextConfig;
