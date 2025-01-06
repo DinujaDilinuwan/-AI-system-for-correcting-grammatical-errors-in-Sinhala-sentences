901618d3e13813c88bc6b8072c565d27d0dad19f
NirupamaD99 <150535823+NirupamaD99@users.noreply.github.com>
2025-01-06 19:48:34 +0530

    final

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import SinhalaGrammarChecker from '@/components/SinhalaGrammarChecker';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <div className="container mx-auto">
+        <SinhalaGrammarChecker />
+      </div>
+    </main>
+  );
+}
